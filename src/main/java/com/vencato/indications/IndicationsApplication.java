78e028d3b204da308d2f89241c841a2683b71635
vencatowilliam <williamvencato@gmail.com>
2022-06-11 13:42:16 -0300

    Initial commit - Created project.

---
+++ src/main/java/com/vencato/indications/IndicationsApplication.java
@@ -0,0 +1,13 @@
+package com.vencato.indications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vencato/indications/IndicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vencato.indications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
