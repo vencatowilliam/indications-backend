33991c53fb375a3a8bc13ce37df86c9cdf9b4eba
vencatowilliam <williamvencato@gmail.com>
2022-06-12 21:45:28 -0300

    Created the dev profile.

---
+++ src/main/java/com/vencato/indications/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.vencato.indications.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.vencato.indications.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+		@Autowired
+		private DBService dbService;
+
+		@Value("${spring.jpa.hibernate.ddl-auto}")
+		private String value;
+
+		@Bean
+		public boolean dbInstantiate() {
+			if(value.equals("create")) {
+				this.dbService.dbInstantiate();
+			}
+			return false;
+		}
+}
