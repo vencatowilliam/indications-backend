549e14f9d4ed1ddd33a16c45bfc352e962d10ac0
vencatowilliam <williamvencato@gmail.com>
2022-06-20 20:05:58 -0300

    Initial data updated.

--- src/main/java/com/vencato/indications/services/DBService.java
+++ src/main/java/com/vencato/indications/services/DBService.java
@@ -40,25 +40,33 @@ public class DBService {
 				LocalDate.parse("01-06-2022", DateTimeFormatter.ofPattern("dd-MM-yyyy")),
 				LocalDate.parse("31-12-2022", DateTimeFormatter.ofPattern("dd-MM-yyyy")), "Nova campanha semestral.");
 
-		Indicator indicator1 = new Indicator(null, "William", DocumentType.CPF, "99283835026", "wv@terra.com.br",
+		Indicator indicator1 = new Indicator(null, "William Pinheiro", DocumentType.CPF, "99283835026", "wp@terra.com.br",
 				51, 981932222, encoder.encode("123"));
-		Indicator indicator2 = new Indicator(null, "Rosane", DocumentType.CPF, "24652625057", "rp@terra.com.br",
+		Indicator indicator2 = new Indicator(null, "Maria da Silva", DocumentType.CPF, "24652625057", "ms@terra.com.br",
 				51, 981934444, encoder.encode("123"));
-		Indicator indicator3 = new Indicator(null, "Gilson", DocumentType.CPF, "16176938040", "gl@terra.com.br",
+		Indicator indicator3 = new Indicator(null, "Joao do Carmo", DocumentType.CPF, "16176938040", "jc@terra.com.br",
+				51, 981935555, encoder.encode("123"));
+		Indicator indicator4 = new Indicator(null, "Silvio Luiz", DocumentType.CPF, "23495853014", "sl@terra.com.br",
+				51, 981935555, encoder.encode("123"));
+		Indicator indicator5 = new Indicator(null, "Odete Soares", DocumentType.CPF, "17520282015", "os@terra.com.br",
+				51, 981935555, encoder.encode("123"));
+		Indicator indicator6 = new Indicator(null, "Mariano Gomes", DocumentType.CPF, "88474201055", "mg@terra.com.br",
 				51, 981935555, encoder.encode("123"));
 
-		Indication indication1 = new Indication(null, "Indicando Angélica, telefone 51996955555", indicator1,
+		Indication indication1 = new Indication(null, "Indicando Angélica Silva, telefone 51996955555", indicator1,
 				campaign1, producer1, null, "Ligar no final da tarde.");
-		Indication indication2 = new Indication(null, "Indicando Noeli, telefone 51996966666", indicator1,
+		Indication indication2 = new Indication(null, "Indicando Ana Moraes, telefone 51996966666", indicator1,
 				campaign1, producer1, null, "Ligar pela manhã.");
-		Indication indication3 = new Indication(null, "Indicando Beti, telefone 51996977777", indicator2,
+		Indication indication3 = new Indication(null, "Indicando Beth Fragoso, telefone 51996977777", indicator2,
 				campaign1, producer1, null, "Ligar no final do mês de Agosto.");
-		Indication indication4 = new Indication(null, "Indicando Belamar, telefone 51996988888", indicator3,
+		Indication indication4 = new Indication(null, "Indicando Belamar Schutel, telefone 51996988888", indicator3,
+				campaign1, producer1, null, "Ligar o mais rápido possível.");
+		Indication indication5 = new Indication(null, "Indicando Amadeu Souza, telefone 51996911111", indicator3,
 				campaign1, producer1, null, "Ligar o mais rápido possível.");
 
 		producerRepository.saveAll(Arrays.asList(producer1));
 		campaignRepository.saveAll(Arrays.asList(campaign1));
-		indicatorRepository.saveAll(Arrays.asList(indicator1,indicator2,indicator3));
-		indicationRepository.saveAll(Arrays.asList(indication1,indication2,indication3,indication4));
+		indicatorRepository.saveAll(Arrays.asList(indicator1,indicator2,indicator3,indicator4,indicator5,indicator6));
+		indicationRepository.saveAll(Arrays.asList(indication1,indication2,indication3,indication4,indication5));
 	}
 }
